eae7a8d85a7f8835d2bfcca6b7d2f6916dec0bb2
Tijmen van den Bos <t.vandenbos@developers.nl>
2019-07-09 15:38:13 +0200

    #6 Refactor index.js to OOP

--- javascript/index.js
+++ javascript/index.js
@@ -1,51 +1,73 @@
-let randomNumber;
+const min = 0;
+const max = 100;
+const randomNumber = generateNumber(min, max);
+
+let playerOne;
+let playerTwo;
+let playerThree;
 
 window.onload = function () {
-  randomNumber = setRandomNumber()
+  playerOne = new Game(document.getElementById('player-one'));
+  playerTwo = new Game(document.getElementById('player-two'));
+  playerThree = new Game(document.getElementById('player-three'));
 };
 
-function setRandomNumber() {
-  return Math.floor(Math.random() * Math.floor(100));
-}
+function Game(gameContainer) {
+  const titleElement = gameContainer.querySelector('.game__title');
+  const inputElement = gameContainer.querySelector('.game__input');
+  const resultElement = gameContainer.querySelector('.game__result');
+  const errorElement = gameContainer.querySelector('.game__error');
 
-function guess(player) {
-  const playerElement = document.getElementById(player);
-  const resultElement = playerElement.querySelector('.game__result');
-  const errorElement = playerElement.querySelector('.game__error');
+  // Set variable values game.
+  inputElement.min = min;
+  inputElement.max = max;
+  titleElement.innerHTML = 'Enter a number between ' + min +  ' and ' + max + '.';
+  errorElement.innerHTML = 'Please enter a number between the ' + min +  ' and ' + max + '.';
 
-  const guess = parseInt(playerElement.querySelector('.game__input').value);
+  // Guess number
+  this.guess = function () {
+    const guess = parseInt(inputElement.value);
 
-  // Default hide error, only show if guess is invalid
-  errorElement.style.display = "none";
-  if (!isValid(guess)) {
-    resultElement.innerHTML = "";
-    errorElement.style.display = "block";
-    return;
-  }
-
-  // Set result
-  if (guess < randomNumber) {
-    resultElement.innerHTML = 'Higher';
-  } else if (guess > randomNumber) {
-    resultElement.innerHTML = 'Lower';
-  } else {
-    resultElement.innerHTML = 'Bingo!!!';
-    endGame();
-  }
-}
-
-function isValid(guess) {
-  const min = 1;
-  const max = 100;
-
-  return Number.isInteger(guess) && guess >= min && guess <= max;
-}
-
-function endGame() {
-  Array.from(document.getElementsByClassName("game")).forEach(
-    function (element) {
-      element.getElementsByClassName("game__input")[0].disabled = true;
-      element.getElementsByClassName("game__submit")[0].disabled = true;
+    // Default hide error, only show if guess is invalid
+    errorElement.style.display = "none";
+    if (!isGuessValid(guess)) {
+      resultElement.innerHTML = "";
+      errorElement.style.display = "block";
+      return;
     }
-  );
+
+    // Set result
+    if (guess < randomNumber) {
+      resultElement.innerHTML = 'Higher';
+    } else if (guess > randomNumber) {
+      resultElement.innerHTML = 'Lower';
+    } else {
+      resultElement.innerHTML = 'Bingo!!!';
+      this.endGame();
+    }
+  };
+
+  // End the game for all players
+  this.endGame = function () {
+    Array.from(document.getElementsByClassName("game")).forEach(
+      function (element) {
+        element.getElementsByClassName("game__input")[0].disabled = true;
+        element.getElementsByClassName("game__submit")[0].disabled = true;
+      }
+    );
+  }
+}
+
+/**
+ * @return {number}
+ */
+function generateNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function isGuessValid(guess) {
+  return Number.isInteger(guess) && guess >= min && guess <= max;
 }
